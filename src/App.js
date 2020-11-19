512ac7ddf394123ce974ec66611ee4b344823eab
sabirhassan <l164258@lhr.nu.edu.pk>
2020-11-19 19:15:49 +0500

    fix: libraries import

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './components/header'
+import {  Route, Switch } from 'react-router-dom';
+
+
+import CategoryList from './components/category_list'
+import SignIn from './components/sign_in'
+import SignUp from './components/sign_up'
+import ProductList from './components/product_list'
+import Cart from './components/cart'
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
+    <div className="container">
+      <Header/>
+
+      <Switch>
+        <Route path="/" component={CategoryList} exact />
+        <Route path="/cart" component={Cart} />
+        <Route path="/signin" component={SignIn} />
+        <Route path="/signup" component={SignUp} />
+        <Route path="/:categoryID" component={ProductList} />
+        
+
+      </Switch>
     </div>
   );
 }
